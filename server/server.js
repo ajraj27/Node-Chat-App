7e7370d3621c21306240a5930aa871ffba3aeac0
ajraj27 <rajanuj.iiita@gmail.com>
2018-08-27 18:04:19 +0530

    Integrated Socket.io to app

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path=require('path');
 const express=require('express');
+const socketIO=require('socket.io');
+const http=require('http');
 
 const publicPath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3000;
 
 const app=express();
+const server=http.createServer(app);
+const io=socketIO(server);
+
+io.on('connection',(socket) => {
+  console.log('New User connected.');
+
+  socket.on('disconnect',() => {
+    console.log('User got disconnected');
+  })
+})
 
 app.use(express.static(publicPath));
 
-app.listen(port,() => {
+server.listen(port,() => {
   console.log('Started app on',port);
 })
