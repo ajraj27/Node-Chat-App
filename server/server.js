e3f82d8b4ef99b38d4613c06e36ba275628f4e53
ajraj27 <rajanuj.iiita@gmail.com>
2018-09-02 14:31:00 +0530

    Added broadcast events

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ const io=socketIO(server);
 io.on('connection',(socket) => {
   console.log('New User connected.');
 
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to the chat room.',
+    createdAt:new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New user joined',
+    createdAt:new Date().getTime()
+  });
+
   socket.on('createMessage',(message) => {
     console.log(message);
     io.emit('newMessage',{
